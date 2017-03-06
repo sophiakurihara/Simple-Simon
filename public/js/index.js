4265b1dd5a62430471484797cbf9ddd97afd4210
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-06 13:56:56 -0600

    GOT MY GAME TO FUNCTION. THANK YOU BEN.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 "use strict";
 // creates empty arrays for Simon generated sequence, and user input.
 var simonSequence = [];
@@ -17,14 +17,14 @@ var buttons = [red, blue, green, yellow];
 
 //generates random sequence for the buttons
 function randomSequence () {
-	var random = Math.floor(Math.random() * 4);
+	return Math.floor(Math.random() * 4);
 }
 
 //appends next button onto simon sequence by using random generator.
 function addToSimon () {
 	var nextButton = randomSequence();
-	console.log(nextButton);
 	simonSequence.push(buttons[nextButton]);
+	console.log(simonSequence);
 }
 
 //updates round upon successful repetition from the user
@@ -32,20 +32,22 @@ function updateRound () {
 	$("#this_round").text(round);
 }
 
-
+//plays the current random sequence generated for this round
 function playSequence() {
 	addToSimon();
 	var index = 0;
 	var max = simonSequence.length;
 	var interval = 2000;
 	var intervalId = setInterval(function() {
-		if (index <= (max-1)) {
+		var button = simonSequence[index];
+		if (index < (max)) {
 			simonSequence[index].animate({
 				opacity: "1.0"
-			}, 430);
-			simonSequence[index].animate({
-				opacity: "0.3"
-			}, 430);
+			}, 430, function(){
+				button.animate({
+					opacity: "0.3"
+				}, 430);
+			});
 			index++;
 		}
 	}, interval);
@@ -65,10 +67,40 @@ function userInput() {
 			userSequence.push(whichButton);
 			var lastUserIndex = userSequence.length-1
 
+			if (userSequence[lastUserIndex] != simonSequence[lastUserIndex]) {
+				userSequence = [];
+				simonSequence = [];
+				round = 0;
+				updateRound();
 
-		})
-	})
+			} else if (userSequence.length == simonSequence.length) {
+				$("#start_text").text("Next Round:");
+				round++;
+				updateRound();
+				userSequence = [];
+
+				buttons.forEach(function (whichButton, index, array) {
+					whichButton.off("click");
+				});
+			playSequence();
+			} else {
+				$("#start_text").text("Doing okay!");
+			}
+
+		});
+	});
 }
 
+	console.log("start game");
+$("#begin_game").click(function() {
+	userSequence = [];
+	simonSequence = [];
+	round = 0;
+	updateRound();
+	playSequence();
 
+	$("#start_text").text("Good luck!")
 });
+
+
+// })();
