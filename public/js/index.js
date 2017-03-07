392d830d01cc6c186034563d589ad312f4e9f8b1
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-06 21:51:16 -0600

    fixed indentations in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,149 +1,157 @@
 
 "use strict";
 $(document).ready(function() {
-// creates empty arrays for Simon generated sequence, and user input.
-var simonSequence = [];
-var userSequence = [];
-// begins game at round 0;
-var round = 0;
 
-// assign variable names to IDs to simplify syntax
-var red = $("#box_red");
-var blue = $("#box_blue");
-var green = $("#box_green");
-var yellow = $("#box_yellow");
+    /* creates empty arrays for Simon generated sequence, and user input.
+    these arrays will be appended to throughout the game*/
+    var simonSequence = [];
+    var userSequence = [];
 
-//creates an array called 'buttons' containing all 4 button colors
-var buttons = [red, blue, green, yellow];
+    // begins game at round 0;
+    var round = 0;
 
-//this function assigns audio files to each button, 
-// and will be played whenever the button is pressed.
-function buttonSounds () {
+    // assign variable names to IDs to simplify syntax
+    var red = $("#box_red");
+    var blue = $("#box_blue");
+    var green = $("#box_green");
+    var yellow = $("#box_yellow");
 
-	red.click(function () {
-		$("#soundbuttonRed").get(0).play();
-		console.log("sound should play");
-	});
+    //creates an array called 'buttons' containing all 4 button colors
+    var buttons = [red, blue, green, yellow];
 
-	blue.click(function () {
-		$("#soundbuttonBlu").get(0).play();
-		console.log("sound should play");
-	});
+    //this function assigns audio files to each button, 
+    // and will be played whenever the button is pressed.
+    function buttonSounds () {
 
-	green.click(function () {
-		$("#soundbuttonGre").get(0).play();
-		console.log("sound should play");
-	});
+        red.click(function () {
+            $("#soundbuttonRed").get(0).play();
+        });
 
-	yellow.click(function() {
-		$("#soundbuttonYel").get(0).play();
-		console.log("sound should play");
-	});
+        blue.click(function () {
+            $("#soundbuttonBlu").get(0).play();
+        });
 
-	}	
+        green.click(function () {
+            $("#soundbuttonGre").get(0).play();
+        });
 
-//generates random sequence for the buttons
-function randomSequence () {
-	return Math.floor(Math.random() * 4);
-}
+        yellow.click(function() {
+            $("#soundbuttonYel").get(0).play();
+        }); 
 
-//appends next button onto simon sequence by using random generator.
-function addToSimon () {
-	var nextButton = randomSequence();
-	simonSequence.push(buttons[nextButton]);
-	console.log(simonSequence);
-}
+        }   
 
-//updates round upon successful repetition from the user
-function updateRound () {
-	$("#this_round").text(round);
-}
+    //generates random order of buttons for the simonSequence
+    function randomSequence () {
+        return Math.floor(Math.random() * 4);
+    }
 
-//displays the random sequence generated for this round
-function playSequence() {
-	addToSimon();
-	var index = 0;
-	var max = simonSequence.length;
-	var interval = 1500;
-	var intervalId = setInterval(function() {
-		var button = simonSequence[index];
-		if (index < (max)) {
-			simonSequence[index].animate({
-				opacity: "1.0"
-			}, 300, function(){
-				button.animate({
-					opacity: "0.3"
-				}, 300);
-			});
-			// appends onto the previous random sequence.
-			index++;
-		}
-	}, interval);
-	//when playSequence is done, checks for userInput
-	userInput();
-}
+    //appends next random button onto simonSequence by using random generator.
+    function addToSimon () {
+        var nextButton = randomSequence();
+        simonSequence.push(buttons[nextButton]);
+        console.log(simonSequence);
+    }
 
-function userInput() {
-	buttonSounds();
-	buttons.forEach(function (whichButton, index, array) {
-		whichButton.click(function () {
-			whichButton.animate({
-				opacity: "1.0"
-			}, 250);
-			whichButton.animate({
-				opacity: "0.3"
-			}, 250);
+    //updates round # upon successful repetition/input from the user
+    function updateRound () {
+        $("#this_round").text(round);
+    }
 
+    /*displays the random sequence generated for this round
+    by lighting up the buttons on the game board*/
+    function playSequence() {
+        addToSimon();
+        var index = 0;
+        var max = simonSequence.length;
+        var interval = 1500;
+        var intervalId = setInterval(function() {
+            var button = simonSequence[index];
+            if (index < (max)) {
+                simonSequence[index].animate({
+                    opacity: "1.0"
+                }, 300, function(){
+                    button.animate({
+                    opacity: "0.3"
+                }, 300);
+                });
+                // appends onto the previous random sequence.
+                index++;
+            }
+        }, interval);
+    //when playSequence is done, checks userInput for equality
+        userInput();
+    }
 
-			userSequence.push(whichButton);
-			var lastUserIndex = userSequence.length-1
+    /* this function dictates what happens when the user presses the buttons and checks
+    for equality between the userSequence and simonSequence*/
+    function userInput() {
+        buttonSounds();
+        buttons.forEach(function (whichButton, index, array) {
+            whichButton.click(function () {
+                whichButton.animate({
+                    opacity: "1.0"
+                }, 250);
+                whichButton.animate({
+                    opacity: "0.3"
+                }, 250);
+                /*whichButton is just a random variable assigned to access the buttons.this function
+                changes button opacity when it is clicked by the user*/
 
-			if (userSequence[lastUserIndex] != simonSequence[lastUserIndex]) {
-				$("#start_text").html("<h2>Better luck next time!</h2>");
-				userSequence = [];
-				simonSequence = [];
-				round = 0;
-				updateRound();
-				setTimeout("location.reload(true);", 2000);
+                userSequence.push(whichButton);
+                //this appends together buttons clicked by the user to create a sequence to be checked
+                
+                var lastUserIndex = userSequence.length-1
+                
+                //if the userSequence input is not the same is the simonSequence
+                if (userSequence[lastUserIndex] != simonSequence[lastUserIndex]) {
+                    $("#start_text").html("<h2>Better luck next time!</h2>");
+                    userSequence = [];
+                    simonSequence = [];
+                    round = 0;
+                    updateRound();
+                    setTimeout("location.reload(true);", 2000);
+                //if the userSequence matches simonSequence 
+                } else if (userSequence.length == simonSequence.length) {
+                    $("#start_text").html("<h2>New Round:</h2>");
+                    round++;
+                    updateRound();
+                    userSequence = [];
 
-			} else if (userSequence.length == simonSequence.length) {
-				$("#start_text").html("<h2>New Round:</h2>");
-				round++;
-				updateRound();
-				userSequence = [];
+                    buttons.forEach(function (whichButton, index, array) {
+                        whichButton.off("click");
+                    });
+                //plays a new sequence for the next round 
+                playSequence();
+                } else {
+                    // gives user this message as long as their sequence is on the right track
+                    $("#start_text").html("<h2>Looking good!</h2>");
+                }
 
-				buttons.forEach(function (whichButton, index, array) {
-					whichButton.off("click");
-				});
-			playSequence();
-			} else {
-				$("#start_text").html("<h2>Looking good!</h2>");
-			}
+            });
+        });
+    }
+    // just a console to check if the game is starting over properly
+    console.log("start game");
+    //refreshes the page when the restart button is clicked
+    $("#restart_game").click(function () {
+        userSequence = [];
+        simonSequence = [];
+        round = 0;
+        updateRound();
+        setTimeout("location.reload(true);", 500);
+    });
+    //initializes the game board when the begin button is clicked
+    $("#begin_game").click(function() {
+        userSequence = [];
+        simonSequence = [];
+        round = 0;
+        updateRound();
+        playSequence();
 
-		});
-	});
-}
-
-	console.log("start game");
-
-$("#restart_game").click(function () {
-	userSequence = [];
-	simonSequence = [];
-	round = 0;
-	updateRound();
-	setTimeout("location.reload(true);", 500);
-});
-
-$("#begin_game").click(function() {
-	userSequence = [];
-	simonSequence = [];
-	round = 0;
-	updateRound();
-	playSequence();
-
-
-	$("#start_text").html("<h2>Good luck!</h2>");
-});
+        //gives the user a good luck message once the game begins!
+        $("#start_text").html("<h2>Good luck!</h2>");
+    });
 
 });
 
